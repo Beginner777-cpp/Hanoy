7e48bee1f5f4a5f4801a752e39bb6ef3f62507ee
Beginner777-cpp <61560747+Beginner777-cpp@users.noreply.github.com>
2020-12-07 18:25:05 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -18,12 +18,67 @@ var btns = document.querySelectorAll('.btn');
 for (let i = 0; i < btns.length; i++) {
     if (i == 0) {
         btns[i].addEventListener('click', () => {
-            if (blocks[0].children.length > 2) {
+            if (blocks[0].children.length > 0) {
                 var temp = document.createElement('div');
                 temp.className = blocks[0].children[0].getAttribute('class');
-                // blocks[1].children[0].appendChild(document.createElement('div'));
-                // blocks[1].setAttribute('class', blocks[0].children[0].getAttribute('class'));
+                if (blocks[1].children.length > 0) {
+                    if (temp.getAttribute('class') > blocks[1].lastChild) {
+                        alert('Нельзя ложить большой элемент на меньший');
+                    }
+                }
+                else {
+                    blocks[0].children[0].remove();
+                    blocks[1].prepend(temp);
+                }
+            }
+        })
+    }
+    if (i == 1) {
+        btns[i].addEventListener('click', () => {
+            if (blocks[0].children.length > 0) {
+                var temp = document.createElement('div');
+                temp.className = blocks[0].children[0].getAttribute('class');
                 blocks[0].children[0].remove();
+                blocks[2].prepend(temp);
+            }
+        })
+    }
+    if (i == 2) {
+        btns[i].addEventListener('click', () => {
+            if (blocks[1].children.length > 0) {
+                var temp = document.createElement('div');
+                temp.className = blocks[1].children[0].getAttribute('class');
+                blocks[1].children[0].remove();
+                blocks[0].prepend(temp);
+            }
+        })
+    }
+    if (i == 3) {
+        btns[i].addEventListener('click', () => {
+            if (blocks[1].children.length > 0) {
+                var temp = document.createElement('div');
+                temp.className = blocks[1].children[0].getAttribute('class');
+                blocks[1].children[0].remove();
+                blocks[2].prepend(temp);
+            }
+        })
+    }
+    if (i == 4) {
+        btns[i].addEventListener('click', () => {
+            if (blocks[2].children.length > 0) {
+                var temp = document.createElement('div');
+                temp.className = blocks[2].children[0].getAttribute('class');
+                blocks[2].children[0].remove();
+                blocks[0].prepend(temp);
+            }
+        })
+    }
+    if (i == 5) {
+        btns[i].addEventListener('click', () => {
+            if (blocks[2].children.length > 0) {
+                var temp = document.createElement('div');
+                temp.className = blocks[2].children[0].getAttribute('class');
+                blocks[2].children[0].remove();
                 blocks[1].prepend(temp);
             }
         })
